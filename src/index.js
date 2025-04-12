e1bccc3a139e98c37f5e2a6916759b4319b73e36
Ammar Saleh <ammar.alammar23@gmail.com>
2025-04-13 01:31:30 +0200

    feat: added PWA support

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client'; // 🆕 notice the /client here
 import App from './App';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+
+serviceWorkerRegistration.register();
 
 // ✅ React 18 way:
 const root = ReactDOM.createRoot(document.getElementById('root'));
